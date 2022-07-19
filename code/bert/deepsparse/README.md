a8559e9d2958999b199a353e1612a25045662cce
Michael Goin <michael@neuralmagic.com>
2022-07-19 12:19:43 -0400

    Initial BERT-Large

---
+++ code/bert/deepsparse/README.md
@@ -0,0 +1,36 @@
+# Neural Magic MLPerf Inference Benchmarks for Natural Language Processing
+
+This is the reference implementation for MLPerf Inference benchmarks for Natural Language Processing.
+
+The chosen model is BERT-Large performing SQuAD v1.1 question answering task.
+
+## Supported Models
+
+| model | framework | accuracy | dataset | model link | precision | notes |
+| ----- | --------- | -------- | ------- | ---------- | --------- | ----- |
+| BERT-Large | ONNX | f1_score=90.6% | SQuAD v1.1 validation set | ["zoo:nlp/question_answering/bert-large/pytorch/huggingface/squad/base-none"](https://sparsezoo.neuralmagic.com/models/nlp%2Fquestion_answering%2Fbert-large%2Fpytorch%2Fhuggingface%2Fsquad%2Fbase-none) | fp32 | This model is the result of fine-tuning the BERT large uncased model on the SQuAD v1.1 datasets. It achieves 90.6% accuracy on the validation dataset. See the included recipe for training instructions. |
+| BERT-Large | ONNX | f1_score=90.3% | SQuAD v1.1 validation set | ["zoo:nlp/question_answering/bert-large/pytorch/huggingface/squad/pruned80_quant-none-vnni"](https://sparsezoo.neuralmagic.com/models/nlp%2Fquestion_answering%2Fbert-large%2Fpytorch%2Fhuggingface%2Fsquad%2Fpruned80_quant-none-vnni) | Fine-tuned based on the PyTorch model and converted with [bert_tf_to_pytorch.py](bert_tf_to_pytorch.py) | int8 | This model is the result of sparse transferring the 80% pruned BERT large uncased model, created using Prune OFA method described in Prune Once for All: Sparse Pre-Trained Language Models, to the SQuAD v1.1 datasets, then quantizing the resulting sparse model. It achieves 90.3% F1 on the validation dataset. See the included recipe for training instructions. |
+
+## Disclaimer
+This benchmark app is a reference implementation that is not meant to be the fastest implementation possible.
+
+## Commands
+
+Please run the following commands:
+
+- `make setup`: initialize submodule, download datasets, and download models.
+- `make build_docker`: build docker image.
+- `make launch_docker`: launch docker container with an interaction session.
+- `python3 run.py --backend=deepsparse --model_path="zoo:nlp/question_answering/bert-large/pytorch/huggingface/squad/pruned80_quant-none-vnni" --scenario=[Offline|SingleStream|MultiStream|Server] [--accuracy]`: run the harness inside the docker container. Performance or Accuracy results will be printed in console.
+
+## Details
+
+- SUT implementation is in [deepsparse_SUT.py](tf_SUT.py). QSL implementation is in [squad_QSL.py](squad_QSL.py).
+- The script [accuracy-squad.py](accuracy-squad.py) parses LoadGen accuracy log, post-processes it, and computes the accuracy.
+- Tokenization and detokenization (post-processing) are not included in the timed path.
+- The inputs to the SUT are `input_ids`, `input_make`, and `segment_ids`. The output from SUT is `start_logits` and `end_logits` concatenated together.
+- `max_seq_length` is 384.
+
+## License
+
+Apache License 2.0
---
+++ code/bert/deepsparse/deepsparse_SUT.py
@@ -0,0 +1,116 @@
+# coding=utf-8
+# Copyright (c) 2020 NVIDIA CORPORATION. All rights reserved.
+# Copyright 2018 The Google AI Language Team Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import array
+import json
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+
+import mlperf_loadgen as lg
+import numpy as np
+from deepsparse import compile_model, Scheduler
+from squad_QSL import get_squad_QSL
+
+
+def latencies_string(latencies_ns):
+    NANO_SEC = 1e9
+    MILLI_SEC = 1000
+
+    latencies_ms = [i / 1e6 for i in latencies_ns]
+    percentiles = [50.0, 80.0, 90.0, 95.0, 99.0, 99.9]
+    buckets = np.percentile(latencies_ms, percentiles).tolist()
+    buckets_str = ",".join(
+        ["{}:{:.4f}".format(p, b) for p, b in zip(percentiles, buckets)]
+    )
+
+    total = np.sum(latencies_ms)
+
+    rv = "qps={:.2f}, mean={:.4f}ms, sum={:.3f}ms, queries={}, percentiles={}".format(
+        len(latencies_ms) / (total / MILLI_SEC),
+        np.mean(latencies_ms),
+        total,
+        len(latencies_ms),
+        buckets_str,
+    )
+    return rv
+
+def batched_list(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        if (i+n) <= len(lst): # last batch will be dealt with outside
+            yield lst[i:i + n]
+
+def scenario_to_scheduler(scenario):
+    if scenario == "SingleStream":
+        return Scheduler.single_stream
+    elif scenario == "Offline":
+        return Scheduler.elastic
+    elif scenario == "Server":
+        return Scheduler.elastic
+    elif scenario == "MultiStream":
+        raise Exception
+    else:
+        raise Exception
+
+class BERT_DeepSparse_SUT():
+    def __init__(self, args):
+        self.profile = args.profile
+        self.model_path = args.model_path
+        self.batch_size = args.batch_size
+        self.scenario = args.scenario
+        self.scheduler = scenario_to_scheduler(args.scenario)
+
+        print("Loading ONNX model...", self.model_path)
+        self.sess = compile_model(self.model_path, batch_size=self.batch_size, scheduler=self.scheduler)
+
+        print("Constructing SUT...")
+        self.sut = lg.ConstructSUT(self.issue_queries, self.flush_queries, self.process_latencies)
+        # self.sut = lg.ConstructSUT(self.issue_queries, self.flush_queries)
+        print("Finished constructing SUT.")
+
+        self.qsl = get_squad_QSL(args.max_examples)
+
+    def pad_to_batch(self, x):
+        x_pad = np.zeros((self.batch_size, x.shape[1]))
+        x_pad[:x.shape[0], :x.shape[1]] = x
+        return x_pad
+
+    def issue_queries(self, query_samples):
+        for i in range(len(query_samples)):
+            eval_features = self.qsl.get_features(query_samples[i].index)
+            fd = [np.array(eval_features.input_ids).astype(np.int64)[np.newaxis, :],
+                  np.array(eval_features.input_mask).astype(np.int64)[np.newaxis, :],
+                  np.array(eval_features.segment_ids).astype(np.int64)[np.newaxis, :]]
+            scores = self.sess.run(fd)
+            output = np.stack(scores, axis=-1)[0]
+
+            response_array = array.array("B", output.tobytes())
+            bi = response_array.buffer_info()
+            response = lg.QuerySampleResponse(query_samples[i].id, bi[0], bi[1])
+            lg.QuerySamplesComplete([response])
+
+    def flush_queries(self):
+        pass
+
+    def process_latencies(self, latencies_ns):
+        print(latencies_string(latencies_ns))
+
+    def __del__(self):
+        print("Finished destroying SUT.")
+
+def get_deepsparse_sut(args):
+    return BERT_DeepSparse_SUT(args)
