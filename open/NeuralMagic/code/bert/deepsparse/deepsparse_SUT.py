a8a64c7073795830fba48de12a63938224394ddc
Michael Goin <michael@neuralmagic.com>
2022-07-29 15:48:28 -0400

    Initial BERT+deepsparse integration

--- open/NeuralMagic/code/bert/deepsparse/deepsparse_SUT.py
+++ open/NeuralMagic/code/bert/deepsparse/deepsparse_SUT.py
@@ -26,28 +26,6 @@ from deepsparse import compile_model, Scheduler
 from squad_QSL import get_squad_QSL
 
 
-def latencies_string(latencies_ns):
-    NANO_SEC = 1e9
-    MILLI_SEC = 1000
-
-    latencies_ms = [i / 1e6 for i in latencies_ns]
-    percentiles = [50.0, 80.0, 90.0, 95.0, 99.0, 99.9]
-    buckets = np.percentile(latencies_ms, percentiles).tolist()
-    buckets_str = ",".join(
-        ["{}:{:.4f}".format(p, b) for p, b in zip(percentiles, buckets)]
-    )
-
-    total = np.sum(latencies_ms)
-
-    rv = "qps={:.2f}, mean={:.4f}ms, sum={:.3f}ms, queries={}, percentiles={}".format(
-        len(latencies_ms) / (total / MILLI_SEC),
-        np.mean(latencies_ms),
-        total,
-        len(latencies_ms),
-        buckets_str,
-    )
-    return rv
-
 def batched_list(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
@@ -58,13 +36,11 @@ def scenario_to_scheduler(scenario):
     if scenario == "SingleStream":
         return Scheduler.single_stream
     elif scenario == "Offline":
-        return Scheduler.elastic
+        return Scheduler.single_stream
     elif scenario == "Server":
-        return Scheduler.elastic
-    elif scenario == "MultiStream":
-        raise Exception
+        return Scheduler.multi_stream
     else:
-        raise Exception
+        raise Exception(scenario)
 
 class BERT_DeepSparse_SUT():
     def __init__(self, args):
@@ -72,14 +48,14 @@ class BERT_DeepSparse_SUT():
         self.model_path = args.model_path
         self.batch_size = args.batch_size
         self.scenario = args.scenario
+        self.responses_sent = 0
         self.scheduler = scenario_to_scheduler(args.scenario)
 
         print("Loading ONNX model...", self.model_path)
         self.sess = compile_model(self.model_path, batch_size=self.batch_size, scheduler=self.scheduler)
 
         print("Constructing SUT...")
-        self.sut = lg.ConstructSUT(self.issue_queries, self.flush_queries, self.process_latencies)
-        # self.sut = lg.ConstructSUT(self.issue_queries, self.flush_queries)
+        self.sut = lg.ConstructSUT(self.issue_queries, self.flush_queries)
         print("Finished constructing SUT.")
 
         self.qsl = get_squad_QSL(args.max_examples)
@@ -89,25 +65,75 @@ class BERT_DeepSparse_SUT():
         x_pad[:x.shape[0], :x.shape[1]] = x
         return x_pad
 
-    def issue_queries(self, query_samples):
-        for i in range(len(query_samples)):
-            eval_features = self.qsl.get_features(query_samples[i].index)
-            fd = [np.array(eval_features.input_ids).astype(np.int64)[np.newaxis, :],
-                  np.array(eval_features.input_mask).astype(np.int64)[np.newaxis, :],
-                  np.array(eval_features.segment_ids).astype(np.int64)[np.newaxis, :]]
-            scores = self.sess.run(fd)
-            output = np.stack(scores, axis=-1)[0]
+    def process_batch(self, batched_features):
+        pad_func = lambda x: self.pad_to_batch(x) if len(batched_features) != self.batch_size else x
+        fd = [
+            pad_func(np.stack(
+                np.asarray([f.input_ids for f in batched_features]).astype(np.int64)[np.newaxis, :])[0, :,
+                         :]),
+            pad_func(np.stack(
+                np.asarray([f.input_mask for f in batched_features]).astype(np.int64)[np.newaxis, :])[0, :,
+                              :]),
+            pad_func(np.stack(
+                np.asarray([f.segment_ids for f in batched_features]).astype(np.int64)[np.newaxis, :])[0,
+                              :, ])
+        ]
+        return fd
 
-            response_array = array.array("B", output.tobytes())
-            bi = response_array.buffer_info()
-            response = lg.QuerySampleResponse(query_samples[i].id, bi[0], bi[1])
-            lg.QuerySamplesComplete([response])
+    def issue_queries(self, query_samples):
+        if self.scenario == "SingleStream" or self.scenario == "Server":
+            for i in range(len(query_samples)):
+                eval_features = self.qsl.get_features(query_samples[i].index)
+                fd = [np.array(eval_features.input_ids).astype(np.int64)[np.newaxis, :],
+                    np.array(eval_features.input_mask).astype(np.int64)[np.newaxis, :],
+                    np.array(eval_features.segment_ids).astype(np.int64)[np.newaxis, :]]
+                scores = self.sess.run(fd)
+                output = np.stack(scores, axis=-1)[0]
+
+                response_array = array.array("B", output.tobytes())
+                bi = response_array.buffer_info()
+                response = lg.QuerySampleResponse(query_samples[i].id, bi[0], bi[1])
+                lg.QuerySamplesComplete([response])
+
+        elif self.scenario == "Offline":
+            #  Extracting features to be split into batches
+            eval_features = [self.qsl.get_features(query_samples[i].index) for i in range(len(query_samples))]
+            for batch_ind, batched_features in enumerate(batched_list(eval_features, self.batch_size)):
+                fd = self.process_batch(batched_features)
+                scores = self.sess.run(fd)
+                output = np.stack(scores, axis=-1)
+
+                # sending responses individually
+                for sample in range(self.batch_size):
+                    response_array = array.array("B", output[sample].tobytes())
+                    bi = response_array.buffer_info()
+                    response = lg.QuerySampleResponse(query_samples[self.responses_sent].id, bi[0], bi[1])
+                    self.responses_sent += 1
+                    lg.QuerySamplesComplete([response])
+
+            # batch remainder in sync
+            last_ind = (batch_ind + 1) * self.batch_size
+            if last_ind < len(eval_features) - 1:
+                batched_features = eval_features[last_ind:]
+                fd = self.process_batch(batched_features)
+                scores = self.sess.run(fd)
+                output = np.stack(scores, axis=-1)[:len(batched_features)]
+                for sample in range(len(output)):
+                    response_array = array.array("B", output[sample].tobytes())
+                    bi = response_array.buffer_info()
+                    response = lg.QuerySampleResponse(query_samples[self.responses_sent].id, bi[0],
+                                                        bi[1])
+                    self.responses_sent += 1
+                    lg.QuerySamplesComplete([response])
+
+        else:
+            raise Exception("Unknown scenario", scenario)
 
     def flush_queries(self):
         pass
 
     def process_latencies(self, latencies_ns):
-        print(latencies_string(latencies_ns))
+        pass
 
     def __del__(self):
         print("Finished destroying SUT.")
